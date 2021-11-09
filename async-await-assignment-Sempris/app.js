2387a10ca86d0f8f17b000827778e91fd0988e47
Wadim Skworzow <sempris2008@gmail.com>
2021-11-09 14:22:09 +0100

    Add files via upload

--- async-await-assignment-Sempris/app.js
+++ async-await-assignment-Sempris/app.js
@@ -9,3 +9,22 @@ function getResult() {
 }
 
 getResult();
+
+// Assignment 2
+
+function randomImg() {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			resolve('https://picsum.photos/200/300?random=1');
+		}, 2000);
+	});
+}
+
+async function getImg() {
+	let result = await randomImg();
+	let img = document.createElement('img');
+	img.src = result;
+	document.body.append(img);
+}
+
+getImg(); // image inside the body   <-- after 2 seconds
